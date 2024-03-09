ce55f48790413cf28d9257819126ba77fe4fe22e
rohan694 <rohansharma996034@gmail.com>
2024-03-09 18:10:48 +0530

    Revert "trying changes"
    This reverts commit 2e7b7acd5fc7c181f9565cf71875af04ff04a695.

--- app.py
+++ app.py
@@ -1,13 +1,7 @@
 from src import create_app
 from dotenv import load_dotenv
-from flask import Flask
-from flask_jwt_extended import JWTManager
-from config import Config
 
-app = Flask(__name__)
-app.config.from_object(Config)
-
-app.config["JWT_SECRET_KEY"] = "super-secret"  # Change this!
+app = create_app()
 load_dotenv('.flaskenv') #the path to your .env file (or any other file of environment variables you want to load)
 
 if __name__ == "__main__":
