f2618f90fa91197cf935f971d7c31bcf22ea7438
rohan694 <rohansharma996034@gmail.com>
2024-03-09 18:10:36 +0530

    Revert "trying change"
    This reverts commit 1db24bb3c2ca39b543bd8b7ac14e22088f06d480.

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
 # from src import create_app
-import os
 from dotenv import load_dotenv
 from flask import Flask
 from flask_jwt_extended import JWTManager
 from config import Config
 
 app = Flask(__name__)
-load_dotenv('.flaskenv') #the path to your .env file (or any other file of environment variables you want to load)
-print("test 16", os.environ.get('MONGO_DB_URL'))
 app.config.from_object(Config)
 
 app.config["JWT_SECRET_KEY"] = "super-secret"  # Change this!
+load_dotenv('.flaskenv') #the path to your .env file (or any other file of environment variables you want to load)
 
 from src.main import bp as main_bp
 app.register_blueprint(main_bp)
